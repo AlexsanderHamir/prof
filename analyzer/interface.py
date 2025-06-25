59ebb2a0e3a1ab63afc3f04a6b7c487e02ee8340
AlexanderGomes <gomes.alexsander.hamir@outlook.com>
2025-06-25 12:33:20 -0700

    Possible Bug & Bad Naming
    1. It doesn't make sense to return an empty slice.
    2. The name of the test file was equal to another one

--- analyzer/interface.py
+++ analyzer/interface.py
@@ -1,4 +1,3 @@
-import sys
 from analyzer.helpers import analyze_all_profiles, validate_benchmark_directories
 from typing import List
 
@@ -8,8 +7,8 @@ from config.config_manager import ConfigManager
 def analyze_profiles(tag: str, profile_types: List[str]):
     config = ConfigManager.get_config()
 
-    benchmark_names = (validate_benchmark_directories(tag) if config.ai_config.all_benchmarks else config.ai_config.specific_benchmarks) or []
-    selected_profile_types = (profile_types if config.ai_config.all_profiles else config.ai_config.specific_profiles) or []
+    benchmark_names = (validate_benchmark_directories(tag) if config.ai_config.all_benchmarks else config.ai_config.specific_benchmarks)
+    selected_profile_types = (profile_types if config.ai_config.all_profiles else config.ai_config.specific_profiles)
 
     print(f"Found {benchmark_names} benchmarks and {selected_profile_types} profile types \n")
 
