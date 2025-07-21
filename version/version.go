1f42b02b27541d2f615e33a51ae79e088fef02fb
AlexanderGomes <gomes.alexsander.hamir@outlook.com>
2025-07-21 11:01:02 -0700

    first version cleanup

--- version/version.go
+++ version/version.go
@@ -11,7 +11,7 @@ import (
 
 // CurrentVersion is the current version of the prof tool.
 const (
-	currentVersion = "1.0.30"
+	currentVersion = "0.0.1"
 	gitHubAPIURL   = "https://api.github.com/repos/AlexsanderHamir/prof/releases/latest"
 	waitTime       = 10
 )
